162a5cf875c5bcccc03d22ebdcaf9f6e9f28e5f5
Scott Morrison <scott.morrison@gmail.com>
2023-12-16 12:36:46 +1100

    experiment: use toolchain for leanprover/lean4#3071

--- test/Mathlib/lakefile.lean
+++ test/Mathlib/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package «repl-mathlib-tests» where
   -- add package configuration options here
-  require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
+  require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "nightly-testing-2023-12-14"
 
 lean_lib «ReplMathlibTests» where
   -- add library configuration options here
