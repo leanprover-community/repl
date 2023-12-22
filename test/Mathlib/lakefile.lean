29ff369cab81067a004c005d01018212205fc2d7
Scott Morrison <scott.morrison@gmail.com>
2023-12-22 11:45:09 +1100

    chore: bump to v4.4.0 (#26)
    * chore: bump to v4.4.0
    
    * fixes

--- test/Mathlib/lakefile.lean
+++ test/Mathlib/lakefile.lean
@@ -5,5 +5,7 @@ package «repl-mathlib-tests» where
   -- add package configuration options here
   require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 
+@[default_target]
 lean_lib «ReplMathlibTests» where
+  globs := #[.submodules `test]
   -- add library configuration options here
--- test/Mathlib/test/H20231214.lean
+++ test/Mathlib/test/H20231214.lean
@@ -3,6 +3,22 @@ open Real
 open Nat
 open BigOperators
 
+/--
+error: type mismatch
+  h₀ x
+has type
+  p x = 2 - x ^ 2 : Prop
+but is expected to have type
+  p x = 6 / x * p x : Prop
+---
+error: unsolved goals
+p q : ℝ → ℝ
+h₀ : ∀ (x : ℝ), p x = 2 - x ^ 2
+h₁ : ∀ (x : ℝ), x ≠ 0 → q x = 6 / x
+hQ : ∀ (x : ℝ), p x = 6 / x
+⊢ p (q 2) = -7
+-/
+#guard_msgs in
 theorem mathd_algebra_35
   (p q : ℝ → ℝ)
   (h₀ : ∀ x, p x = 2 - x^2)
