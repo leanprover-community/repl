b9bbaa17b1e04cf2bbc708b325946d14420d2e94
Scott Morrison <scott.morrison@gmail.com>
2024-02-29 15:34:10 +1100

    chore: bump toolchain to v4.6.0

--- test/Mathlib/lakefile.lean
+++ test/Mathlib/lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 package «repl-mathlib-tests» where
   -- add package configuration options here
-  require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
+  require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.6.0"
 
 @[default_target]
 lean_lib «ReplMathlibTests» where
