8bdb5dbf8fcd1222c36e6d5ded93f7a8fdd08a3f
Matěj Kripner <kripnermatej@gmail.com>
2025-02-21 16:55:28 +0100

    Allow extraction of only top-level infotrees.

--- REPL/Main.lean
+++ REPL/Main.lean
@@ -232,6 +232,10 @@ def runCommand (s : Command) : M IO (CommandResponse ⊕ Error) := do
   | some "tactics" => trees.flatMap InfoTree.retainTacticInfo
   | some "original" => trees.flatMap InfoTree.retainTacticInfo |>.flatMap InfoTree.retainOriginal
   | some "substantive" => trees.flatMap InfoTree.retainTacticInfo |>.flatMap InfoTree.retainSubstantive
+  | some "no_children" => trees.map fun t => match t with
+    | InfoTree.context _ _ => t
+    | InfoTree.node i _ => InfoTree.node i (PersistentArray.empty)
+    | InfoTree.hole _ => t
   | _ => []
   let infotree ← if jsonTrees.isEmpty then
     pure none
