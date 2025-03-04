bf24c08d1c364863c741046ef6e931a8dc931dde
Kim Morrison <kim@tqft.net>
2025-03-04 11:18:08 +1100

    fix tests

--- REPL/Main.lean
+++ REPL/Main.lean
@@ -212,7 +212,6 @@ def runCommand (s : Command) : M IO (CommandResponse ⊕ Error) := do
     cmdContext := (cmdSnapshot?.map fun c => c.cmdContext).getD
       { fileName := "",
         fileMap := default,
-        -- tacticCache? := none,
         snap? := none,
         cancelTk? := none } }
   let env ← recordCommandSnapshot cmdSnapshot
